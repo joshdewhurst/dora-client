1fc387692b8f9e2e770120f1e09e0899c08ebe11
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-08 19:53:37 -0700

    styled register and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,38 +2,34 @@ import { Link, BrowserRouter } from 'react-router-dom'
 
 export default function Navbar({ currentUser, handleLogout }) {
 	 const loggedIn = (
-		<>
-			
-
-			
-
+		<div className='mx-auto w-1/2 inline-block'>
 			<Link to='/home'>Home Feed</Link> { " | "}
 			<Link to='/trending'>Trending Now</Link> { " | "}
 			<Link to='/search'>Search</Link> { " | "}
 			{/* if the user is logged in... */}
 			<Link to="/"> <span onClick={handleLogout}>Logout</span> </Link> { " | "}
 			<Link to="/profile">Profile</Link> { " | "}
-		</>
+		</div>
 	 )
 
 	 const loggedOut = (
-		<>
+		<div className='mx-auto w-1/2 inline-block'>
 			{/* if the user is not logged in... */}
 			<Link to="/register">
 				register
 			</Link>
 
-			<Link to="/login">
+			<Link className='ml-3' to="/login">
 				login
 			</Link>
-		</>
+		</div>
 	 )
 
 	return (
-		<nav>
+		<nav className='mx-auto text-4xl w-fit font-bold mb-5'>
 			{/* user always sees this section */}
-			<Link to="/">
-				<p>User App</p>
+			<Link className='inline-block mr-12' to="/">
+				<p>d'ora</p>
 			</Link>
 
 			{currentUser ? loggedIn : loggedOut}
