ebccabb746b0d7daaeb3ff221d7e3ea1fc6fac16
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-10 09:45:53 -0700

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,19 @@ import { Link, BrowserRouter } from 'react-router-dom'
 
 export default function Navbar({ currentUser, handleLogout }) {
 	 const loggedIn = (
-		<div className='mx-auto w-1/2 inline-block'>
-			<Link to='/home'>Home Feed</Link> { " | "}
-			<Link to='/trending'>Trending Now</Link> { " | "}
+		<div className='mx-auto w-3/4 inline-block flex-nowrap'>
+			<Link to='/home'>Feed</Link> { " | "}
+			<Link to='/trending'>Trending</Link> { " | "}
 			<Link to='/search'>Search</Link> { " | "}
 			{/* if the user is logged in... */}
-			<Link to="/"> <span onClick={handleLogout}>Logout</span> </Link> { " | "}
-			<Link to="/profile">Profile</Link> { " | "}
+			<Link to="/profile">Profile</Link>{ " | "}
+			<Link to="/"> <span onClick={handleLogout}>Logout</span> </Link> 
 		</div>
 	 )
 
 	 const loggedOut = (
-		<div className='mx-auto w-1/2 inline-block'>
+		<div className='mx-auto w-fit inline-block'>
+			
 			{/* if the user is not logged in... */}
 			<Link to="/register">
 				register
@@ -26,13 +27,13 @@ export default function Navbar({ currentUser, handleLogout }) {
 	 )
 
 	return (
-		<nav className='mx-auto text-4xl w-fit font-bold mb-5'>
-			{/* user always sees this section */}
-			<Link className='inline-block mr-12' to="/">
-				<p>d'ora</p>
-			</Link>
-
-			{currentUser ? loggedIn : loggedOut}
+		<nav className='bg-blue-600'>
+			<div className='mx-auto text-4xl w-fit  whitespace-nowrap font-bold mb-5 p-5 text-white'>
+				<Link className='inline-block mr-12' to="/">
+					<p>d'ora</p>
+				</Link>
+				{currentUser ? loggedIn : loggedOut}
+			</div>
 		</nav>
 	)
 }
