aab536950d62d61dd2c31bc527402cfa319d3a88
tylerchan33 <tylerchan33@gmail.com>
2022-10-10 18:13:56 -0700

    styling media and trending

--- src/components/pages/Media.jsx
+++ src/components/pages/Media.jsx
@@ -1,3 +1,4 @@
+import { getSpaceUntilMaxLength } from "@testing-library/user-event/dist/utils"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { isCompositeComponent } from "react-dom/test-utils"
@@ -8,18 +9,24 @@ export default function Media (props) {
     const { name, artist } = useParams()
     const [media, setMedia] = useState('')
     const [singer, setSinger] = useState('')
+    const [image, setImage] = useState('')
+    
 
     useEffect(() => {
         const mediaSearch = async () => {
             try {
                 const trackSearchUrl =  `http://ws.audioscrobbler.com/2.0/?method=track.search&track=${name}&api_key=${process.env.REACT_APP_API_KEY}&format=json`
                 const artistSearchUrl = `http://ws.audioscrobbler.com//2.0/?method=artist.search&artist=${artist}&api_key=${process.env.REACT_APP_API_KEY}&format=json`
+                const image = `https://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key=${process.env.REACT_APP_API_KEY}&artist=${artist}&track=${name}&format=json`
                 const mediaResp = await axios.get(trackSearchUrl)
                 const singerResp = await axios.get(artistSearchUrl)
+                const imageResp = await axios.get(image)
                 // console.log(mediaResp)
                 // console.log(singerResp)
                 setSinger(singerResp.data.results.artistmatches.artist[0])
                 setMedia(mediaResp.data.results.trackmatches.track[0])
+                setImage(imageResp.data.track.album.image[3]["#text"])
+                
 
             } catch(err) {
                 console.warn(err)
@@ -36,12 +43,16 @@ export default function Media (props) {
                 {media.name}
                 {media.artist}
                 {media.listeners}
+                
 
             </div>
             <div>
                 <h1>singer info</h1>
                 {singer.name}
                 {singer.listeners}
+            </div>
+            <div>
+                <img class="mx-auto" src={image} alt={`${media.name} album cover`}></img>
             </div>
             
         </div>
--- src/components/pages/Trending.jsx
+++ src/components/pages/Trending.jsx
@@ -22,23 +22,24 @@ export default function Trending(props) {
   
     const topTracks = props.trending.map((track, i) => {
         return(
+            
             <div key={`track${i}`} className='w-2/3 text-3xl flex justify-around p-4 mx-auto'>
-                    <div className="w-2/5 text-right">
-                        {track.name}
-                    </div>
-                        -
-                    <div className="w-2/5 text-left flex justify-between">
-                        <p> {track.artist.name}</p>
-                        <Link className="text-sm text-blue-900" to={`../media/${track.name}/${track.artist.name}`}>Details</Link>
-                        
-                    </div>
-            </div>
+                <div className="w-2/5 text-right">
+                    {track.name}
+                </div>
+                    -
+                <div className="w-2/5 text-left flex justify-between">
+                    <p> {track.artist.name}</p>
+                    <Link className="text-sm text-blue-900" to={`../media/${track.name}/${track.artist.name}`}>Details</Link>
+                </div>
+            </div>       
         )
     })
     return(
         <div className="mb-10">
             <h1 className="text-5xl font-bold p-5">Top 50 Trending Songs</h1>
             <div>
+                
                 <h4>{topTracks}</h4>
             </div>
         </div>
