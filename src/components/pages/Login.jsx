99f451501aeb69865645e32aad1e1521b50e6034
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-12 10:23:35 -0700

    login and register styled.

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -46,31 +46,31 @@ export default function Login({ currentUser, setCurrentUser }) {
 	}
 
 	return (
-		<div>
-			<h1>Login to Your Account:</h1>
-
-			<p>{msg}</p>
-
+		<div className="bg-blue-700 p-12 rounded-md mt-12 flex flex-col text-white text-2xl w-2/3 mx-auto">
 			<form onSubmit={handleSubmit}>
-				<label htmlFor='email'>Email:</label>
-				<input 
-					type="email"
-					id="email"
-					placeholder='your email...'
-					onChange={e => setEmail(e.target.value)}
-					value={email}
-				/>
+				<div className='font-bold w-1/3 flex flex-row justify-start mb-2 uppercase'>
+					<h3>welcome back</h3>
+				</div>
+				<div className="flex flex-row justify-around uppercase rounded-t-md text-black bg-indigo-200  font-bold p-2">
+					<div className='flex w-1/2 flex-col p-2'>
+						<label className="mb-2 border-b-4 border-yellow-400" htmlFor='email'>Email:</label>
+						<input className='pl-2' type="email" id="email" placeholder='email' onChange={e => setEmail(e.target.value)} value={email} />
+					</div>
 
-				<label htmlFor='password'>Password:</label>
-				<input 
-					type="password"
-					id="password"
-					placeholder='password...'
-					onChange={e => setPassword(e.target.value)}
-					value={password}
-				/>
+					<div className='flex w-1/2 flex-col p-2'>
+						<label className='mb-2 border-b-4 border-red-400' htmlFor='password'>Password:</label>
+						<input className='pl-2' type="password" id="password" placeholder='password' onChange={e => setPassword(e.target.value)} value={password} />
+					</div>
 
-				<button type="submit">Login</button>
+				</div>
+				<div className="p-4 bg-blue-900 rounded-b-md">
+					<h1 className='text-2xl font-bold'>Login to your account:</h1>
+						<p>{msg}</p>
+				</div>
+
+				<div className='mt-8 flex justify-center'>
+					<button className="bg-sky-500 hover:bg-sky-700 rounded-md p-2 font-bold" type="submit">Login</button>
+				</div>
 			</form>
 		</div>
 	)
